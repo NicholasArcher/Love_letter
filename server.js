788e9c6fe5776d1d78b34ab44dfebf98bc962ad0
Raspberry-Jams <archernick2568@gmail.com>
2019-04-06 13:38:01 -0400

    Created an http server to host files and a Web Socket server to communicate

---
+++ server.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var express = require("express");
+var WebSocket = require("ws");
+var app = express();
+
+var server = app.listen(3000, () => {console.log('Server is running on port 3000')
+});
+var wss = new WebSocket.Server({server});
+
+app.use(express.static(__dirname));
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {console.log(message)});
+    console.log("Connection established");
+})
+
+
